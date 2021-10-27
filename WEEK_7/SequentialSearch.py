b30f56ab08474445a9beb3a12fa161d6c71f7061
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-10-27 09:59:43 +0700

    Create SequentialSearch.py

---
+++ WEEK_7/SequentialSearch.py
@@ -0,0 +1,15 @@
+def Sequential_Search(dlist, item):
+    
+    pos = 0
+    found = False
+    
+    while pos < len(dlist) and not found:
+        if dlist[pos] == item:
+            found = True
+        else:
+            pos = pos +1
+            
+    return found, pos
+  
+print(Sequential_Search([11,23,58,31,56,77,43,12,65,19],31))
+print(Sequential_Search(['a','b','c','d','e','f','g','h'],'h'))
