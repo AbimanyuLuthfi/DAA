00f3bfd9c32716a58d37ee567a18c432a02b8604
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-10-27 12:33:23 +0700

    Create Tugas3PageRank.py

---
+++ WEEK_9/Tugas3PageRank.py
@@ -0,0 +1,27 @@
+# Tugas 3
+myWeb = nx.DiGraph()
+myPages = range(1,4)
+
+connections = [(1,2), (2,1), (2,3), (3,2), (3,4), (4,3)]
+myWeb.add_nodes_from(myPages)
+myWeb.add_edges_from(connections)
+
+pos = nx.kamada_kawai_layout(myWeb)
+nx.draw(myWeb, pos, arrows = True, with_labels = True)
+plt.show()
+
+def createPageRank(aGraph) :
+    nodes_set = len(aGraph)
+    M = nx.to_numpy_matrix(aGraph)
+    outwards = np.squeeze(np.asarray(np.sum(M, axis = 1)))
+    prob_outwards = np.array (
+        [1.0/count
+        if count > 0 else 0.0 for count in outwards])
+    G = np.asarray(np.multiply(M.T, prob_outwards))
+    p = np.ones(nodes_set) / float (nodes_set)
+    if np.min(np.sum(G, axis = 0)) < 1.0 :
+        print ('WARN : G is substochastic')
+    return G,p
+
+G, p = createPageRank(myWeb)
+print (G)
