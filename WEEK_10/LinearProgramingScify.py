107b8ed47a307767184f3a2669b6437e11d62dc1
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-11-03 12:08:58 +0700

    Create LinearProgramingScify.py

---
+++ WEEK_10/LinearProgramingScify.py
@@ -0,0 +1,23 @@
+# Import Required Libraries
+import numpy as np
+from scipy.optimize import linprog
+
+# Set The Inequality Constraints Matrix
+# Note : The Inequality Constraints Must Be In The Form Of <=
+A = np.array([[1,0], [2,3], [1,1], [-1,0], [0,-1]])
+
+# Set The Inequality Contraints Vector
+b = np.array([16, 19, 8, 0, 0])
+
+# Set The Coefficients Of The linear Objective Function Vector
+# Note : When Maximizing, Change The Signs Of the c Vector Coefficient
+c = np.array([-5,-7])
+
+# Solve Linear Programming Problem
+res = linprog(c, A_ub = A, b_ub = b)
+
+# print Results
+print('Optimal value :', round(res.fun * -1, ndigits = 2),
+     '\nx values :', res.x,
+     '\nNumber of iterations performed :', res.nit,
+     '\nStatus :', res.message)
