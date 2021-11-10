e02207a412f44e2addf08428d883624e25385c6c
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-11-10 12:49:21 +0700

    Create Lat2Graph_Element.py

---
+++ WEEK_11/Lat2Graph_Element.py
@@ -0,0 +1,11 @@
+# Latihan 2
+graph = {"R" : ["L", "O"],
+         "O" : ["R", "M", "P"],
+         "M" : ["N", "O"],
+         "L" : ["R", "P"],
+         "P" : ["L", "N", "O"],
+         "N" : ["M", "P"]
+        }
+
+# Print the graph
+print(graph)
