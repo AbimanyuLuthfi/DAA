5ba855a352fe9b1f4de4f350eb0197d1a7b601d3
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-11-10 12:57:12 +0700

    Create AddVertex_ClassGraph.py

---
+++ WEEK_11/AddVertex_ClassGraph.py
@@ -0,0 +1,27 @@
+class graph :
+    
+    def __init__(self,gdict = None) :
+        if gdict is None :
+            gdict = {}
+        self.gdict = gdict
+        
+    def getVertices(self) :
+        return list(self.gdict.keys())
+    
+# Add the vertex as a key
+    def addVertex(self, vrtx):
+        if vrtx not in self.gdict:
+            self.gdict[vrtx] = []
+
+# Create the dictionary with graph elements
+graph_elements = { "a" : ["b", "c"],
+                 "b" : ["a", "d"],
+                 "c" : ["a", "d"],
+                 "d" : ["e"],
+                 "e" : ["d"]
+                 }
+
+g = graph(graph_elements)
+g. addVertex("f")
+g. addVertex("g")
+print(g.getVertices())
