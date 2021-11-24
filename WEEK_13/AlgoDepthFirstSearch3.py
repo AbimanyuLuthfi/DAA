21ccecea0ed9a611c24caa306d9870b302405577
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-11-24 10:47:16 +0700

    Create AlgoDepthFirstSearch3.py

---
+++ WEEK_13/AlgoDepthFirstSearch3.py
@@ -0,0 +1,26 @@
+def dfs (graph, start, visited = None) :
+    if visited is None :
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next in graph[start] - visited :
+        dfs(graph, next, visited)
+    return visited
+  
+  graph = {'Amin' : {'Wasim', 'Nick', 'Mike'},
+        'Wasim' : {'Imran', 'Amin'},
+        'Imran' : {'Wasim', 'Faras'},
+        'Faras' : {'Imran'},
+        'Mike' : {'Amin'},
+        'Nick' : {'Amin'}}
+  
+# O amin ------------------
+# |              |          |
+# O wasim       O nick     O mike  
+# |
+# O imran
+# |
+# O faras
+# naik dari faras ke amin, geser ke nick & mike
+
+dfs (graph, 'Faras')
